7dea3e950731a1ac84e6d2a616d6ec5f0e5c48ae
Christopher Dunn <cdunn@pacificbiosciences.com>
2017-04-09 14:54:35 -0700

    Always 100000 size msa_array, while we figure out virtual memory problem

--- falcon.nim
+++ falcon.nim
@@ -349,9 +349,9 @@ proc get_cns_from_align_tags*(tag_seqs: var seq[ref align_tags_t]; n_tag_seqs: s
   var msa_array: ref msa_pos_t
   block:
     # Ensure msa_array exists.
-    #let size = 100000
-    #assert(t_len < 100000)
-    let size = tlen + 1
+    let size = 100000
+    assert(t_len < 100000)
+    #let size = tlen + 1
     if msa_array == nil:
       msa_array = get_msa_working_sapce(cast[cuint](size))
   defer:
